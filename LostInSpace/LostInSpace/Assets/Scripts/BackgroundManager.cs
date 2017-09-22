8f43161a4c29f50cef9ccee4a4a7c0ba6aa45026
David Bibb <kaweekagames@gmail.com>
2017-09-21 23:22:53 -0700

    Player, Enemy Movement plus other stuff

---
+++ LostInSpace/LostInSpace/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour {
+
+    public GameObject background;
+    public int sizeY;
+    public int sizeX;
+
+    private Transform playerTrans;
+    private List<GameObject> backgroundObjects = new List<GameObject>();
+    private Vector2 startPosition;
+    private SpriteRenderer spriteRenderer;
+    private int regenerateFactor;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        spriteRenderer = background.GetComponent<SpriteRenderer>();
+        startPosition = new Vector2(transform.position.x - (sizeX / 2 * spriteRenderer.bounds.size.x), transform.position.y - (sizeY / 2 * spriteRenderer.bounds.size.y));
+        regenerateFactor = sizeX - 3;
+
+        for (int x = 0; x < sizeX; x++)
+        {
+            for(int y = 0; y < sizeY; y++)
+            {
+                Vector3 newPosition = new Vector3(startPosition.x + spriteRenderer.bounds.size.x * x, startPosition.y + spriteRenderer.bounds.size.y * y, 0);
+                int z = Random.Range(0, 4);
+                GameObject bgObject = Instantiate(background, newPosition, Quaternion.Euler(0, 0, (90 * z)));
+                backgroundObjects.Add(bgObject);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		foreach(GameObject n in backgroundObjects)
+        {
+            if(n.transform.position.x > (playerTrans.position.x + spriteRenderer.bounds.size.x * regenerateFactor))
+            {
+                n.transform.position = new Vector2(n.transform.position.x - spriteRenderer.bounds.size.x * sizeX, n.transform.position.y);
+            }
+
+            if (n.transform.position.x < (playerTrans.position.x - spriteRenderer.bounds.size.x * regenerateFactor))
+            {
+                n.transform.position = new Vector2(n.transform.position.x + spriteRenderer.bounds.size.x * sizeX, n.transform.position.y);
+            }
+
+            if (n.transform.position.y > (playerTrans.position.y + spriteRenderer.bounds.size.y * regenerateFactor))
+            {
+                n.transform.position = new Vector2(n.transform.position.x, n.transform.position.y - spriteRenderer.bounds.size.y * sizeY);
+            }
+
+            if (n.transform.position.y < (playerTrans.position.y - spriteRenderer.bounds.size.y * regenerateFactor))
+            {
+                n.transform.position = new Vector2(n.transform.position.x, n.transform.position.y + spriteRenderer.bounds.size.y * sizeY);
+            }
+        }
+	}
+}
---
+++ LostInSpace/LostInSpace/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float forwardSpeed;
+    public float maxSpeed;
+    public float rotationSpeed;
+    public float slowDownTime;
+
+    private float speed = 0;
+
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Rotation control
+        float zRotation = Input.GetAxisRaw("Horizontal");
+
+        zRotation = -zRotation;
+
+        transform.Rotate(0, 0, zRotation * rotationSpeed * Time.deltaTime);
+
+
+        //Forward movement control
+        if (Input.GetButtonDown("Forward"))
+        {
+            speed += forwardSpeed;
+            
+        }
+
+        speed -= slowDownTime * Time.deltaTime;
+
+        speed = Mathf.Clamp(speed, 0, maxSpeed);
+
+        transform.Translate(0, speed * Time.deltaTime, 0);
+
+    }
+}
